a9438346a3f615ae2a76b13a50ed66ad0988e3d4
Binh Trinh <trinhthaibinh.ecom@gmail.com>
2019-08-13 06:52:02 -0700

    Set up graphQl Yogo Server, and create first Query, Mutation

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,13 @@
-const mutations = {};
+const Mutations = {
+  createDog(parent, args, ctx, info) {
+    global.dogs = global.dogs || [];
 
-module.exports = mutations;
+    // Create a dog!
+    const newDog = { name: args.name };
+    global.dogs.push(newDog);
+    return newDog;
+    console.log(args);
+  }
+};
+
+module.exports = Mutations;
