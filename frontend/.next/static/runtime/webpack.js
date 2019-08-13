161ebdf1a74ab104877f968a659240c4baa4c6a0
Binh Trinh <trinhthaibinh.ecom@gmail.com>
2019-08-13 02:09:16 -0700

    global stylking and nav styling

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f356fe782f9a109c8db5";
+/******/ 	var hotCurrentHash = "e87cd1b376ba1ed26216";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
