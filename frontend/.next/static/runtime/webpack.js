16f5f71609d478587f216dc00e840bf5c14019be
Binh Trinh <trinhthaibinh.ecom@gmail.com>
2019-08-13 02:38:03 -0700

    Visualize Route Changes with NProgress

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "e87cd1b376ba1ed26216";
+/******/ 	var hotCurrentHash = "94f7b42a48f0b68cee38";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
