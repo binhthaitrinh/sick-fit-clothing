c5a60937c5ccb496f53c4ac7bcb95b97ba1d9359
Binh Trinh <trinhthaibinh.ecom@gmail.com>
2019-08-15 21:15:37 -0700

    persist update permissions in database

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "b55a5ace496fa192f865";
+/******/ 	var hotCurrentHash = "2ffdcea3adcbf59e319d";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
