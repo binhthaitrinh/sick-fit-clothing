2460e05313f558c71c15824079ad89b30baf6494
Binh Trinh <trinhthaibinh.ecom@gmail.com>
2019-08-15 23:57:18 -0700

    Display car items and totals

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "2717ee4888b46a0b3f9c";
+/******/ 	var hotCurrentHash = "0e16afbc8847e2be9e74";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
