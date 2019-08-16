0a07dc2ce4f4de41c04b761c3071fd099d4edb75
Binh Trinh <trinhthaibinh.ecom@gmail.com>
2019-08-15 23:28:51 -0700

    Server side add to cart

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -239,6 +239,48 @@ const Mutations = {
       },
       info
     );
+  },
+
+  async addToCart(parent, args, ctx, info) {
+    // make sure they are signed in
+    const userId = ctx.request.userId;
+    if (!userId) {
+      throw new Error('You must be signed in to do that');
+    }
+    // query users current cart
+    const [existingCartItem] = await ctx.db.query.cartItems({
+      where: {
+        user: { id: userId },
+        item: { id: args.id }
+      }
+    });
+
+    // check if that item is already in their cart and ++ if it is
+    if (existingCartItem) {
+      console.log('This item is already in the car');
+      return ctx.db.mutation.updateCartItem(
+        {
+          where: { id: existingCartItem.id },
+          data: { quantity: existingCartItem.quantity + 1 }
+        },
+        info
+      );
+    }
+
+    // if its not, create a fresh CartItem for that user
+    return ctx.db.mutation.createCartItem(
+      {
+        data: {
+          user: {
+            connect: { id: userId }
+          },
+          item: {
+            connect: { id: args.id }
+          }
+        }
+      },
+      info
+    );
   }
 };
 
--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "6ac9127b13caabec90ab";
+/******/ 	var hotCurrentHash = "2717ee4888b46a0b3f9c";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
